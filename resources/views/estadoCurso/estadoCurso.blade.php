f45db68ceb1e18e57be276622a47da0ee44eb6a2
DanielGarcia097 <2009402600101@ingenieria.usac.edu.gt>
2018-08-16 21:39:58 -0600

    V 1.0.5
    Eliminación de paginación innecesaria en la vista estado curso.

--- resources/views/estadoCurso/estadoCurso.blade.php
+++ resources/views/estadoCurso/estadoCurso.blade.php
@@ -58,15 +58,7 @@
                                         @endforeach
 									</tbody>
 								</table>
-								<ul class="pagination pagination-sm">
-								  	<li class="disabled"><a href="#!">«</a></li>
-								  	<li class="active"><a href="#!">1</a></li>
-								  	<li><a href="#!">2</a></li>
-								  	<li><a href="#!">3</a></li>
-								  	<li><a href="#!">4</a></li>
-								  	<li><a href="#!">5</a></li>
-								  	<li><a href="#!">»</a></li>
-								</ul>
+								
 							</div>
 					  	</div>
                           <div class="tab-pane fade" id="list1">
@@ -98,15 +90,7 @@
 										
 									</tbody>
 								</table>
-								<ul class="pagination pagination-sm">
-								  	<li class="disabled"><a href="#!">«</a></li>
-								  	<li class="active"><a href="#!">1</a></li>
-								  	<li><a href="#!">2</a></li>
-								  	<li><a href="#!">3</a></li>
-								  	<li><a href="#!">4</a></li>
-								  	<li><a href="#!">5</a></li>
-								  	<li><a href="#!">»</a></li>
-								</ul>
+								
 							</div>
 					  	</div>
                           <div class="tab-pane fade" id="list2">
@@ -141,15 +125,7 @@
 										</tr>
 									</tbody>
 								</table>
-								<ul class="pagination pagination-sm">
-								  	<li class="disabled"><a href="#!">«</a></li>
-								  	<li class="active"><a href="#!">1</a></li>
-								  	<li><a href="#!">2</a></li>
-								  	<li><a href="#!">3</a></li>
-								  	<li><a href="#!">4</a></li>
-								  	<li><a href="#!">5</a></li>
-								  	<li><a href="#!">»</a></li>
-								</ul>
+								
 							</div>
 					  	</div>
 					</div>
