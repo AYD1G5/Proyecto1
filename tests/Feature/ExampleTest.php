a8bdbe80df41611817aa9f6d32b763a688e07ef2
DanielGarcia097 <2009402600101@ingenieria.usac.edu.gt>
2018-08-24 11:40:05 -0600

    V 2.1.1
    Se realizaron pruebas unitarias a cada una de las vistas de la aplicación, para verificar que está cargando correctamente.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -23,4 +23,104 @@ class ExampleTest extends TestCase
       $response = $this->get('/pruebaUsuariosCursoCarrera/1/1/1');
       $response->assertStatus(200)->assertSee('Usuario: 1 Curso: 1 Carrera: 1');
     }
+
+
+    
+    public function testAsignaciontemporal(){
+        $response = $this->call('POST', '/login', [
+        'email' => 'willyslider@gmail.com',
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+    $response = $this->get('/asignaciontemporal');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testAsignaciontemporalCreate(){
+        $response = $this->call('POST', '/login', [
+        'email' => 'willyslider@gmail.com',
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+    $response = $this->get('/asignaciontemporal/1/1/create');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+    
+    public function testAsignaciontemporalMostrar(){
+        $response = $this->call('POST', '/login', [
+        'email' => 'willyslider@gmail.com',
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+    $response = $this->get('asignaciontemporal/1/mostrar');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+
+    public function testRevisarAsignacion(){
+        $response = $this->call('POST', '/login', [
+        'email' => 'willyslider@gmail.com',
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+    $response = $this->get('revisarasignacion/1');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+    
+
+
+    public function testFinalizarAsignacion(){
+        $response = $this->call('POST', '/login', [
+        'email' => 'willyslider@gmail.com',
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+    $response = $this->get('finalizarasignacion/');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+
+    public function testCursosPorSemestre(){
+        $response = $this->call('POST', '/login', [
+        'email' => 'willyslider@gmail.com',
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+    $response = $this->get('/cursosporsemestre');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+    
+
+    public function testCursosPorSemestreID(){
+        $response = $this->call('POST', '/login', [
+        'email' => 'willyslider@gmail.com',
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+    $response = $this->get('/cursosporsemestre/1/semestre');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+    
+
+    public function testReportCursosGanados(){
+        $response = $this->call('POST', '/login', [
+        'email' => 'willyslider@gmail.com',
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+    $response = $this->get('/ReporteCursosGanados');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+    
+
+    public function testReporteCursosObligaPendientes(){
+        $response = $this->call('POST', '/login', [
+        'email' => 'willyslider@gmail.com',
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+    $response = $this->get('/ReporteCursosObligaPendientes');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+        
 }
