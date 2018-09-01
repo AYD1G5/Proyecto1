b8675ffec783091c3790f9d1eee8fb9e4cec575d
DanielGarcia097 <2009402600101@ingenieria.usac.edu.gt>
2018-08-31 22:28:40 -0600

    Test de Vista PerfilCatedratico
    Se creó la prueba a realizar para la vista de Pefil de Catedratico.

---
+++ tests/Unit/PerfilCatedraticoTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PerfilCatedraticoTest extends TestCase
+{
+    
+
+    /**
+     * ARCHIVO DE PRUEBAS UNITARIAS EXLUSIVO 
+     * PARA LA VISTA DE PERFIL CATEDRATICO----
+     * 
+     * 
+     * Primer prueba unitaria en metodología TDD
+     * 
+     * 
+     * Prueba para la vista de perfil de catedratico.
+     * Se verifica que la respuesta de la vista 
+     * sea de 200, que significa que se abrió correctamente.
+     */
+    public function testAsignaciontemporalMostrar(){
+        $response = $this->call('POST', '/login', [
+        'email' => 'danielgarcia0976@gmail.com',
+        'password' => 'informatica10',
+        '_token' => csrf_token()
+    ]);
+    $response = $this->get('/PerfilCatedratico');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
