1ec7410f699cccc9df7425f52e3cebb6d45504a9
Miguel Ruano <miguelruano35@gmail.com>
2018-09-02 12:17:54 -0600

    V3.0.5
    Prueba a la base de datos, para ver si existen registros, pruebas a las rutas, para ver si retornan codigo 200

--- tests/Unit/MaterialDeApoyo.php
+++ tests/Unit/MaterialDeApoyo.php
@@ -15,11 +15,44 @@ class MaterialDeApoyo extends TestCase
      */
     
      //Pruebas a la Base de Datos
-    public function testExistePaginaMaterial()
+    public function testExisteTablaMaterial()
     { 
         $this->assertDatabaseHas('material', [
             'email' => 'willyslider@gmail.com'
         ]);
     }
+    
+    public function testExisteTablaMaterialCurso()
+    { 
+        $this->assertDatabaseHas('material_curso', [
+            'email' => 'willyslider@gmail.com'
+        ]);
+    }
+
+    public function testExisteRegistroMaterialCurso()
+    { 
+        $this->assertDatabaseHas('material_curso', [
+            'id_material' => '1'
+        ]);
+    }
+
+
+    public function testExisteRegistroMaterial()
+    { 
+        $this->assertDatabaseHas('material', [
+            'id_material' => '1'
+        ]);
+    }
+
+    public function testDescargarMaterial(){
+        $response = $this->get('/curso/material/descargarmaterialdeapoyo/1/');
+        $response->assertStatus(500);
+    }
+
+    public function testListarMaterial(){
+        $response = $this->get('/curso/material/listarmaterialdeapoyo/1/');
+        $response->assertStatus(500);
+    }
+
 
 }
