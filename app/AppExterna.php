15e3984dd43298f4042c98882d1d9c8b2878f13f
WilliamLopez5 <willyslider@gmail.com>
2018-09-11 21:54:37 -0600

    V4.0.2
    Generación de Archivos para la migraciones y el modelo de la tabla AppExterna

---
+++ app/AppExterna.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AppExterna extends Model
+{
+    /**
+     * modelo para realizar consultas a la base de datos en la tabla AppExterna    
+    */
+    protected $table = 'AppExterna';
+    
+    protected $primaryKey='id_AppExterna';
+    
+    protected $fillable = [
+        'nombre'
+    ];
+
+}
