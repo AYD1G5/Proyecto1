29e291e7e4bf784c4ff823619ef8adeb01732c51
deahtom123 <201504200@ingenieria.usac.edu.gt>
2018-09-16 16:25:20 -0600

    V4.0.5
    Se Agrego informacion adicional a la vista

--- app/Http/Controllers/BuscadorPersonasController.php
+++ app/Http/Controllers/BuscadorPersonasController.php
@@ -8,6 +8,26 @@ class BuscadorPersonasController extends Controller
 {
     //
     public function BuscadorPersonas(){
+        /*$personas=DB::table('users as u')
+        ->where('u.id_rol', '=', '2')
+        ->select('u.id as id', 'u.registro_academico as registro', 'u.nombre as nombre_persona',
+                'u.apellido as apellido_persona', 'u.direccion as direccion_persona', 'u.email as email_persona')
+                
+        ->get();
+         /** INICIALIZAR LA COLECCION DE SALIDA */
+        /*$personasCollection = new Collection();
+        $str = ''; 
+         foreach ($personas as &$persona) {
+             $objetopersona = new Objetopersona();
+            $objetopersona->id_persona = $persona->id;
+            $objetopersona->registro_academico = $persona->registro;
+            $objetopersona->nombre_persona = $persona->nombre_persona;
+            $objetopersona->apellido_persona = $persona->apellido_persona;
+            $objetopersona->direccion_persona = $persona->direccion_persona;
+            $objetopersona->email_persona = $persona->email_persona;
+            $personasCollection->push($objetopersona);
+        }
+        return view('BuscadorPersonas')->with("arreglo",$personasCollection);*/
         return view('BuscadorPersonas');
     }
 }
--- resources/views/BuscadorPersonas.blade.php
+++ resources/views/BuscadorPersonas.blade.php
@@ -20,25 +20,43 @@
 						<button type="submit" class="btn btn-default">BUSCAR</button>
 						</form>
 					</ul>
-					<div id="myTabContent" class="tab-content">
-					  	<div class="tab-pane fade active in" id="list">
-							<div class="table-responsive">
- 							</div>
-					  	</div>
-					</div>
-					<ul class="nav nav-tabs" style="margin-bottom: 15px;">
-					  	<li class="active"><a href="#list" data-toggle="tab"></a></li>
+					
                        
 					
 					
 					<div id="myTabContent" class="tab-content">
 					  	<div class="tab-pane fade active in" id="list">
 							<div class="table-responsive">
-								
+							<table class="table table-hover text-center">
+									<thead>
+										<tr>
+											
+                                            <th class="text-center">Registro</th>
+											<th class="text-center">Nombre</th>
+											<th class="text-center">Apellido</th>
+											<th class="text-center">Direccion</th>
+											<th class="text-center">Email</th>
+											
+										</tr>
+									</thead>
+									<tbody>
+                                    
+                                    {{--
+										@foreach ($arreglo as $elemento)
+                                            
+                                            <td>{{$elemento->registro_academico}}</td>
+	  										<td>{{$elemento->nombre_catedratico}}</td>
+											<td>{{$elemento->apellido_catedratico}}</td>
+                                            <td>{{$elemento->direccion_catedratico}}</td>
+											<td>{{$elemento->email_catedratico}}</td>
+                                            
+										</tr>
+                                        @endforeach
+										--}}
+									</tbody>
+								</table>
 							</div>
 					  	</div>
-                    
- 						  
 							</div>
 					  	</div>
 					</div>
