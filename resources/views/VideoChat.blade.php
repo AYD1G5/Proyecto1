f5ad83adb91dde0129dd1a35b8c122bdc1aee2ac
mikeruano3 <miguelruano35@gmail.com>
2018-09-17 23:59:22 -0600

    V4.0.11
    Modificacion de la vista de Video conferencia

--- resources/views/VideoChat.blade.php
+++ resources/views/VideoChat.blade.php
@@ -1,1 +1,9 @@
-<iframe src="https://tokbox.com/embed/embed/ot-embed.js?embedId=160ac79a-46fa-43ed-9289-a49d15287517&room=DEFAULT_ROOM&iframe=true" width=1000 height=800 allow="microphone; camera" ></iframe>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+    <iframe src="https://tokbox.com/embed/embed/ot-embed.js?embedId=160ac79a-46fa-43ed-9289-a49d15287517&room=DEFAULT_ROOM&iframe=true" width=1000 height=800 allow="microphone; camera" ></iframe>
+    </div>
+</div>
+@endsection
