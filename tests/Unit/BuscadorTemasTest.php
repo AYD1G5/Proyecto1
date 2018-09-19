9212f42bda88710e562aac1d3ab24b045062c905
ElmerReal <elmerreal@hotmail.com>
2018-09-18 21:56:57 -0600

    V4.0.0
    Se creo el archivo y la primera prueba unitaria

---
+++ tests/Unit/BuscadorTemasTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BuscadorTemasTest extends TestCase
+{
+    /**
+     * ARCHIVO DE PRUEBAS UNITARIAS EXLUSIVO 
+     * PARA LA VISTA DE BUSCADOR DE Personas----
+     * 
+     * 
+     * Primer prueba unitaria en metodología TDD
+     * 
+     * 
+     * Prueba para la vista de buscador de personas.
+     * Se verifica que la respuesta de la vista 
+     * sea de 200, que significa que se abrió correctamente.
+     * 
+     * 
+     * 
+     */
+    public function testBuscadorTemas(){
+        $response = $this->call('POST', '/login', [
+        'email' => 'willyslider@gmail.com',
+        'password' => '12345678',
+        '_token' => csrf_token()
+        ]);
+    $response = $this->get('/BuscadorTemas');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
