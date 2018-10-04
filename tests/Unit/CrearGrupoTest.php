ecfbc34f37ed1de6a597c8cccf5cf9eebf1f4a3f
WilliamLopez5 <willyslider@gmail.com>
2018-10-03 22:27:05 -0600

    V5.0.3
    (TDD) Crear Prueba para observar si se esta dirigiendo correctamente a la pagina Crear Grupo

--- tests/Unit/CrearGrupoTest.php
+++ tests/Unit/CrearGrupoTest.php
@@ -38,6 +38,33 @@ class CrearGrupoTest extends TestCase
         $this->assertEquals(200, $response->getStatusCode());
     }
 
+
+     /**
+     * Prueba creada para probar si esta retornando correctamente la vista \CrearGrupo
+     */
+    public function testVerTextoCrearGrupo(){
+        $user=new User();
+        $user->registro_academico='55555';
+        $user->nombre='Name';
+        $user->apellido='LastName';
+        $user->id_rol='1';
+        $user->direccion='Guatemala';
+        $user->email='Name@gmail.com';
+        $user->password=Hash::make('12345678');
+        $user->telefono='777777';
+        $user->save();
+        
+        $response = $this->call('POST', '/login', [
+        'email' => $user->email,
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+
+    $response = $this->get('/CrearGrupo');
+    $user->delete();
+    $response->assertSeeText('Nuevo Grupo');
+    }
+
     /**
      * Prueba creada para probar la respueta de la base de datos de la tabla: Grupo
      * y la columna: Nombre
