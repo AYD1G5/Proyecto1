d6020ab11ef2d03c48d321847c712c95f8c56bcd
deahtom123 <201504200@ingenieria.usac.edu.gt>
2018-10-05 21:33:53 -0600

    V5.0.1
    Creacion de la vista y ruta de buscar tema

--- app/Http/Controllers/ListarTemasController.php
+++ app/Http/Controllers/ListarTemasController.php
@@ -7,4 +7,9 @@ use Illuminate\Http\Request;
 class ListarTemasController extends Controller
 {
     //
+    public function index()
+    {      
+         
+        return view('ListarTemas');
+    }
 }
---
+++ resources/views/ListarTemas.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout1')
+@section('content')
+<!-- Content page -->
+<div class="container-fluid">
+    <div class="page-header">
+        <h1 class="text-titles"><i class="zmdi zmdi-book zmdi-hc-fw"></i> <small>LISTAR TEMAS</small></h1>
+    </div>
+    <center><p class="lead">LISTAR TEMAS</p></center>
+</div>
+@endsection 
