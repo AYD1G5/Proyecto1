57c3afbebcfe2b8a0c79a639865c95f157019104
deahtom123 <201504200@ingenieria.usac.edu.gt>
2018-10-07 19:50:25 -0600

    V5.0.2
    Se crearon las primeras pruebas de crear tema

--- tests/Unit/CrearTemasTest.php
+++ tests/Unit/CrearTemasTest.php
@@ -5,7 +5,11 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Chrome;
+use App\Http\Controllers\CrearTemasController;
 class CrearTemasTest extends TestCase
 {
     /**
@@ -13,8 +17,35 @@ class CrearTemasTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
-    {
-        $this->assertTrue(true);
+    public function CrearTemaView(){
+        $response = $this->call('POST', '/login', [
+        'email' => 'willyslider@gmail.com',
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+    $response = $this->get('/CrearTemas');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+     /**
+     * Prueba para verificar que el tema 1 existe en la base de datos.
+     *
+     * @return void
+     */
+    public function testTablaTema()
+    { 
+        $this->assertDatabaseHas('temas', [
+            'tema' => '1',
+        ]);
+    }
+  /**
+     * Prueba para saber si el creador del tema tiene id 1
+     *
+     * @return void
+     */
+    public function testTablaTemaCreador()
+    { 
+        $this->assertDatabaseHas('temas', [
+            'creador_id' => '1',
+        ]);
     }
 }
