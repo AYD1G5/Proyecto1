323b14f5625ccab1a209d46fcecd1931bf74b373
DanielGarcia097 <2009402600101@ingenieria.usac.edu.gt>
2018-10-09 19:02:40 -0600

    V5.0.4
    Creación de consulta desde controlador con la lista de temas.

--- app/Http/Controllers/ListarTemasController.php
+++ app/Http/Controllers/ListarTemasController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Tema;
+
 use Illuminate\Http\Request;
 
 class ListarTemasController extends Controller
@@ -10,6 +12,9 @@ class ListarTemasController extends Controller
     public function index()
     {      
          
-        return view('ListarTemas');
+
+        $temas = Tema::all();
+        dd($temas);
+        return view('ListarTemas')->with('temas', $temas);
     }
 }
