142f306bcab55f7ee38c51028cb92d27e7210a0c
deahtom123 <201504200@ingenieria.usac.edu.gt>
2018-10-15 21:53:55 -0600

    6.0.2
    Creacion de metodo vacios de pruebas

--- tests/Unit/ReportarBloquearUsuariosTest.php
+++ tests/Unit/ReportarBloquearUsuariosTest.php
@@ -19,5 +19,32 @@ class ReportarBloquearUsuariosTest extends TestCase
     {
         $this->assertTrue(true);
     }
+       /**
+     * Esta prueba es para ver si el usuario esta en la base de datos
+     */
+    public function testExisteUsuario(){   
+
+    }
+     /**
+     * Verifica si la contrasena es correcta para iniciar sesion
+     */
+
+    public function testCredencialesCorrectas(){
+
+    }
+
+    /**
+     * Verifica si un usuario esta desbloqueado
+     */
+    public function testUsuarioDesbloqueado(){
+
+    }
+
+     /**
+     * Verifica si un usuario esta bloquedo
+     */
+    public function testUsuarioBloqueado(){
+
+    }
   
 }
