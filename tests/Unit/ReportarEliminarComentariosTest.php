a02af6c72206cdcd61729598ff6bb6803d8646a8
ElmerReal <elmerreal@hotmail.com>
2018-10-16 16:07:25 -0600

    V6.0.3
    En esta version se agrego el codigo de las pruebas a realizar TDD

--- tests/Unit/ReportarEliminarComentariosTest.php
+++ tests/Unit/ReportarEliminarComentariosTest.php
@@ -18,7 +18,6 @@ class ReportarEliminarComentariosTest extends TestCase
         $this->assertTrue(true);
     }
 
-
         /**
         * Esta prueba se encarga de agregar
         * un comentario al grupo de comentarios
@@ -27,7 +26,22 @@ class ReportarEliminarComentariosTest extends TestCase
         
         public function testReportarComentario()
         {
-            $this->assertTrue(true);
+            $curso =1;
+            $creador = 1;
+            $titulo = "Tema Prueba unitaria";
+            $descripcion = "Esta es la descripcion del tema";
+           $controlador = new PerfilTemaController();
+           $Tema = $controlador->CrearTema($curso,$creador,$titulo,$descripcion);
+           $controlador1=new ComentarioTema();
+           $contenidoComentario = "Este sistema no sirve";
+           $controlador1->CrearComentario($Tema->tema, $contenidoComentario);
+           $controlador1->ReportarComentario($comentario->id);
+           $comentario = ComentarioTema::where(array(
+            'comentariotema_id' => $comentario
+            ))->first();
+            $this->assertEquals($comentario->reportado, 1);
+            $comentario->delete();
+            $Tema->delete();
         }
         
 
@@ -38,7 +52,18 @@ class ReportarEliminarComentariosTest extends TestCase
         */
         public function testReportarTema()
         {
-            $this->assertTrue(true);
+            $curso =1;
+            $creador = 1;
+            $titulo = "Tema Prueba unitaria";
+            $descripcion = "Esta es la descripcion del tema";
+           $controlador = new PerfilTemaController();
+           $Tema = $controlador->CrearTema($curso,$creador,$titulo,$descripcion);
+          $controlador -> ReportarTema($Tema->tema);
+           $Tema1 = Tema::where(array(
+            'tema' => $Tema->tema
+            ))->first();
+            $this->assertEquals($Tema1->reportado, 1);
+            $Tema1->delete();
         }
 
         /**
@@ -49,7 +74,15 @@ class ReportarEliminarComentariosTest extends TestCase
         */
         public function testReportarMaterial()
         {
-            $this->assertTrue(true);
+            $nombreMaterial="Material de apoyo XXX";
+            $controlador = new MaterialDeApoyoController();
+            $MaterialTemp = $controlador->CrearMaterial($nombreMaterial);
+            $controlador ->ReportarMaterial($MaterialTemp->id_material);
+            $MaterialTemp1 = Material::where(array(
+                'id_material' =>$MaterialTemp->id_material
+                ))->first();
+            $this->assertEquals($MaterialTemp1->reportado, 1);
+            $MaterialTemp1->delete();
         }
 
          /**
@@ -60,7 +93,23 @@ class ReportarEliminarComentariosTest extends TestCase
 
         public function testQuitarReporteComentario()
         {
-            $this->assertTrue(true);
+            $curso =1;
+            $creador = 1;
+            $titulo = "Tema Prueba unitaria";
+            $descripcion = "Esta es la descripcion del tema";
+           $controlador = new PerfilTemaController();
+           $Tema = $controlador->CrearTema($curso,$creador,$titulo,$descripcion);
+           $controlador1=new ComentarioTema();
+           $contenidoComentario = "Este sistema no sirve";
+           $controlador1->CrearComentario($Tema->tema, $contenidoComentario);
+           $controlador1->ReportarComentario($comentario->id);
+           $controlador1->QuitarReporteComentario($comentario->id);
+           $comentario = ComentarioTema::where(array(
+            'comentariotema_id' => $comentario
+            ))->first();
+            $this->assertEquals($comentario->reportado, 0);
+            $comentario->delete();
+            $Tema->delete();
         }
 
         /**
@@ -71,7 +120,19 @@ class ReportarEliminarComentariosTest extends TestCase
 
         public function testQuitarReporteTema()
         {
-            $this->assertTrue(true);
+            $curso =1;
+            $creador = 1;
+            $titulo = "Tema Prueba unitaria";
+            $descripcion = "Esta es la descripcion del tema";
+           $controlador = new PerfilTemaController();
+           $Tema = $controlador->CrearTema($curso,$creador,$titulo,$descripcion);
+          $controlador -> ReportarTema($Tema->tema);
+          $controlador -> QuitarReporteTema($Tema->tema);
+           $Tema1 = Tema::where(array(
+            'tema' => $Tema->tema
+            ))->first();
+            $this->assertEquals($Tema1->reportado, 1);
+            $Tema1->delete();
         }
 
           /**
@@ -83,6 +144,15 @@ class ReportarEliminarComentariosTest extends TestCase
 
         public function testQuitarReporteMaterial()
         {
-            $this->assertTrue(true);
+            $nombreMaterial="Material de apoyo XXX";
+            $controlador = new MaterialDeApoyoController();
+            $MaterialTemp = $controlador->CrearMaterial($nombreMaterial);
+            $controlador ->ReportarMaterial($MaterialTemp->id_material);
+            $controlador ->QuitarReporteMaterial($MaterialTemp->id_material);
+            $MaterialTemp1 = Material::where(array(
+                'id_material' =>$MaterialTemp->id_material
+                ))->first();
+            $this->assertEquals($MaterialTemp1->reportado, 0);
+            $MaterialTemp1->delete();
         }
 }
