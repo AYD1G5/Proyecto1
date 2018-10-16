b9ad6fbdbbf5600da3e3c7ee02cbdb6bae9ab282
ElmerReal <elmerreal@hotmail.com>
2018-10-15 22:02:53 -0600

    V6.0.2
    En esta version se agrego la descripcion de las pruebas a realizar a esta funcionalidad

--- tests/Unit/ReportarEliminarComentariosTest.php
+++ tests/Unit/ReportarEliminarComentariosTest.php
@@ -17,4 +17,72 @@ class ReportarEliminarComentariosTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+
+        /**
+        * Esta prueba se encarga de agregar
+        * un comentario al grupo de comentarios
+        * reportados por contenido inapropiado.
+        */
+        
+        public function testReportarComentario()
+        {
+            $this->assertTrue(true);
+        }
+        
+
+        /**
+        * Esta prueba se encarga de agregar
+        * un tema al grupo de temas
+        * reportados por contenido inapropiado.
+        */
+        public function testReportarTema()
+        {
+            $this->assertTrue(true);
+        }
+
+        /**
+        * Esta prueba se encarga de agregar
+        * un material de apoyo al grupo de material
+        * de apoyo
+        * reportados por contenido inapropiado.
+        */
+        public function testReportarMaterial()
+        {
+            $this->assertTrue(true);
+        }
+
+         /**
+        * Esta prueba se encarga de quitar
+        * un comentario al grupo de comentarios
+        * reportados por contenido inapropiado.
+        */
+
+        public function testQuitarReporteComentario()
+        {
+            $this->assertTrue(true);
+        }
+
+        /**
+        * Esta prueba se encarga de quitar
+        * un tema al grupo de temas
+        * reportados por contenido inapropiado.
+        */
+
+        public function testQuitarReporteTema()
+        {
+            $this->assertTrue(true);
+        }
+
+          /**
+        * Esta prueba se encarga de quitar
+        * un material de apoyo al grupo de  material de apoyo
+        * reportados por contenido inapropiado.
+        */
+
+
+        public function testQuitarReporteMaterial()
+        {
+            $this->assertTrue(true);
+        }
 }
