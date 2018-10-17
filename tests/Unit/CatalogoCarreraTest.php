361bc85dc646247f8657e468a1cb57e3c2a80b32
DanielGarcia097 <2009402600101@ingenieria.usac.edu.gt>
2018-10-16 19:00:02 -0600

    V6.0.1
    (TDD) creación de pruebas para verificar que la DB y la Vista de Catalago Carrera

--- tests/Unit/CatalogoCarreraTest.php
+++ tests/Unit/CatalogoCarreraTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
+use App\Carrera;
 use Auth;
 use Illuminate\Support\Facades\Hash;
 
@@ -39,5 +40,70 @@ class CatalogoCarreraTest extends TestCase
     $user->delete();
         $this->assertEquals(200, $response->getStatusCode());
     }
+
+     /**
+     * Prueba creada para probar la respueta de la base de datos de la tabla: Carreras
+     * y la columna: Nombre_carrera
+     */
+    public function testCatalogoCarreraDB()
+    {
+        $user=new User();
+        $user->registro_academico='555557-1';
+        $user->nombre='Name';
+        $user->apellido='LastName';
+        $user->id_rol='1';
+        $user->direccion='Guatemala';
+        $user->email='Name71@gmail.com';
+        $user->password=Hash::make('12345678');
+        $user->telefono='777777';
+        $user->save();
+
+        $response = $this->call('POST', '/login', [
+        'email' => $user->email,
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+
+
+        $carrera=new Carrera();
+        $carrera->codigo_carrera='2555';
+        $carrera->nombre_carrera='NuevaCarrera';
+        $carrera->save();
+
+        $this->assertDatabaseHas('carrera', [
+            'nombre_carrera' => 'NuevaCarrera'
+        ]);
+
+        $carrera->delete();
+        $user->delete();
+    }
+
+     /**
+     * Prueba creada para probar si esta retornando correctamente la vista \CatalogoCarrera
+     */
+    public function testVerTextoCrearGrupo(){
+        $user=new User();
+        $user->registro_academico='555557-1';
+        $user->nombre='Name';
+        $user->apellido='LastName';
+        $user->id_rol='1';
+        $user->direccion='Guatemala';
+        $user->email='Name71@gmail.com';
+        $user->password=Hash::make('12345678');
+        $user->telefono='777777';
+        $user->save();
+
+        $response = $this->call('POST', '/login', [
+        'email' => $user->email,
+        'password' => '12345678',
+        '_token' => csrf_token()
+    ]);
+
+
+    $response = $this->get('/CatalogoCarrera');
+    $user->delete();
+    $response->assertSeeText('Catalogo Carrera');
+    }
+
 
 }
