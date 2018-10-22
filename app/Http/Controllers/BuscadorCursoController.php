97912e86833e3182bf0fc6afe6410f6566216663
mikeruano3 <miguelruano35@gmail.com>
2018-10-21 21:18:02 -0600

    V6.0.3
    Agregar pagina de blade, controlador

--- app/Http/Controllers/BuscadorCursoController.php
+++ app/Http/Controllers/BuscadorCursoController.php
@@ -23,6 +23,25 @@ class BuscadorCursoController extends Controller
             return view('BuscadorCurso',["cursos"=>$cursos,"searchText"=>$query]);
         }
     }
+
+    public function PlanRepitencia(Request $request){
+        if($request)
+        {     
+            $query = trim($request->get('searchText'));
+            $cursos=DB::table('curso as c')
+            ->join('area as a', 'a.id_area', '=', 'c.id_area')
+            ->join('escuela as e', 'e.id_escuela', '=', 'c.id_escuela')
+            ->join('curso_pensum as cp', 'cp.id_curso', '=', 'c.id_curso')
+            ->join('curso_asignacion as ca', 'ca.id_curso_pensum', '=', 'cp.id_curso_pensum')
+            ->where('c.nombre_curso', 'LIKE', '%'.$query.'%')
+            ->groupBy('c.id_curso')
+            ->select('c.id_curso as id', 'c.codigo_curso as codigo_curso', 'c.nombre_curso as nombre_curso',
+                    'cp.id_curso_pensum', 'e.nombre_escuela as escuela', 'a.nombre_area as area')
+                    
+            ->get();
+            return view('BuscadorCurso',["cursos"=>$cursos,"searchText"=>$query]);
+        }
+    }
 }
 
 
